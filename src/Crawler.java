234442dc7ef93c0bd15bb4ea6f196b76e36c294d
Hajesha <hkandasamy@ryerson.ca>
2020-04-03 01:12:22 -0400

    get login cookies

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,71 +1,43 @@
-
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
  
 import org.jsoup.Jsoup;
+import org.jsoup.Connection.Method;
 import org.jsoup.Connection.Response;
 import org.jsoup.nodes.Document;
 
 public class Crawler {
 	
-	public static void main(String[] args) {
-	        try {
-	            
-	        	String searchWebsiteURL = "https://mangadex.org/search";
-	            //grab login form page first
-	            Response loginPageResponse = 
-	                    Jsoup.connect(searchWebsiteURL)
-	                    .referrer("http://www.rediff.com/")
-	                    .userAgent("Mozilla/5.0")
-	                    .timeout(10 * 1000)
-	                    .followRedirects(true)
-	                    .execute();
-	            
-	            System.out.println("Fetched login page");
-	            
-	            //get the cookies from the response, which we will post to the action URL
-	            Map<String, String> mapLoginPageCookies = loginPageResponse.cookies();
-	            
-	            //lets make data map containing all the parameters and its values found in the form
-	            Map<String, String> mapParams = new HashMap<String, String>();
-	            mapParams.put("FormName", "existing");
-	            mapParams.put("seclogin", "on");
-	            mapParams.put("login", "YOUR_USER_ID");
-	            mapParams.put("passwd", "YOUR_PASSWORD");
-	            mapParams.put("remember", "1");
-	            mapParams.put("proceed", "Go");
-	            
-	            //URL found in form's action attribute
-	            String strActionURL = searchWebsiteURL;
-	            
-	            Response responsePostLogin = Jsoup.connect(strActionURL) 
-	                    .referrer(searchWebsiteURL)
-	                    .userAgent("Mozilla/5.0")
-	                    .timeout(10 * 1000)
-	                    .data(mapParams)
-	                    .cookies(mapLoginPageCookies)
-	                    .followRedirects(true)
-	                    .execute();
-	            
-	            System.out.println("HTTP Status Code: " + responsePostLogin.statusCode());
-	            
-	            //parse the document from response
-	            Document document = responsePostLogin.parse();
-	            System.out.println(document);
-	            
-	            //get the cookies
-	            Map<String, String> mapLoggedInCookies = responsePostLogin.cookies();
-	            
-	            /*
-	             * For all the subsequent requests, you need to send
-	             * the mapLoggedInCookies containing cookies
-	             */
-	            
-	        } catch (IOException e) {
-	            // TODO Auto-generated catch block
-	            e.printStackTrace();
-	        }
-	        
-	    }
+	public static void main(String[] args) throws Exception {
+	    
+		System.out.println(login().parse().html());
+	    
+	}
+	
+	public static Response login() throws Exception {
+		final String USER_AGENT = "\"Mozilla/5.0 (Windows NT\" +\n" +  
+	            "          \" 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2\"";  
+	    String loginFormUrl = "https://mangadex.org/search";  
+	    String loginActionUrl = "https://mangadex.org//ajax/actions.ajax.php?function=login&nojs=1";  
+	    
+	    HashMap<String, String> cookies = new HashMap<>();  
+	    HashMap<String, String> formData = new HashMap<>(); 
+	    
+	    Response loginForm = Jsoup.connect(loginFormUrl).
+	    		method(Method.GET).
+	    		userAgent(USER_AGENT).execute(); 
+	    
+	    cookies.putAll(loginForm.cookies());
+	    
+	    formData.put("login_username", Environments.username);  
+	    formData.put("login_password", Environments.password);  
+	    formData.put("two_factor", "");  
+
+	    return 	Jsoup.connect(loginActionUrl)  
+	            .cookies(cookies)  
+	            .data(formData)  
+	            .method(Method.POST)  
+	            .userAgent(USER_AGENT)  
+	            .execute();  
+	}
 }
