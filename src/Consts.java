a21fbcfbb58fd4f2913eb6a06691409318384e47
Hajesha <hkandasamy@ryerson.ca>
2020-04-04 03:03:30 -0400

    re-utilize cookie save cookie on device correct parsing on searchpage

---
+++ src/Consts.java
@@ -0,0 +1,9 @@
+
+public class Consts {
+
+    final static String fileName = "mangaDexCookie.file";
+    final static String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0";  
+    final static String loginPage = "https://mangadex.org//ajax/actions.ajax.php?function=login&nojs=1"; 
+    final static String searchPage = "https://mangadex.org/search"; 
+    final static String rememberMeTokenName = "mangadex_rememberme_token";
+}
--- src/Crawler.java
+++ src/Crawler.java
@@ -1,7 +1,12 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
- 
+import java.util.Map;
+
 import org.jsoup.Jsoup;
 import org.jsoup.Connection.Method;
 import org.jsoup.Connection.Response;
@@ -12,53 +17,111 @@ import org.jsoup.select.Elements;
 public class Crawler {
 	
 	public static void main(String[] args) throws Exception {
-		getTitlesOnPage(login());
-		
+		getTitlesOnPage(searchPage());
 	}
+	
 	public static ArrayList<MangaOverview> getTitlesOnPage(Document page){
 		Elements mangas = page.body().select("div.manga-entry");
-		ArrayList listOfManga = new ArrayList();
+		ArrayList<MangaOverview> listOfManga = new ArrayList<MangaOverview>();
 		
 		for (Element manga: mangas) {
-			listOfManga.add(new Manga());
+			Element imageElement = manga.selectFirst("div.rounded");
+			String imageURL = manga.selectFirst("img").attr("src");
+			Element titleElement = manga.selectFirst("a.manga_title");
+	
+			//String url = image.absUrl("src"); absolute path
+			String description = manga.select("div").last().text();
+			String title = manga.selectFirst("a.manga_title").attr("title");
+			String url = titleElement.attr("href");
+			listOfManga.add(new MangaOverview(title,imageURL,description,url));
 		}
 		return listOfManga;
 	}
 	
-	public static Document login() throws Exception {
-		final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0";  
-	    String loginFormUrl = "https://mangadex.org/search";  
-	    String loginActionUrl = "https://mangadex.org//ajax/actions.ajax.php?function=login&nojs=1";  
-	    
+	public static Document searchPage() throws Exception {
+		boolean loggedIn = false;
+		boolean bypass = false;
+		Response home = null;
+		Document landingPage = null;
+		while (!loggedIn) {
+			home = Jsoup.connect(Consts.searchPage)
+					.cookies(login(bypass))
+					.userAgent(Consts.userAgent)
+					.execute();
+			
+			 landingPage = home.parse();
+			if (landingPage.selectFirst("#login")!= null) {
+				bypass = !bypass;
+			} else {
+				loggedIn = true;
+			}
+		}
+
+		//TODO: add null check
+		return landingPage;
+	}
+	
+	public static Map<String, String> login(boolean byPassSavedCookie) throws Exception {
+		if (!byPassSavedCookie) {
+			Map<String,String> cookies = getCookie();
+			if (cookies != null) {
+				return cookies;
+			}
+		}
+		
 	    HashMap<String, String> cookies = new HashMap<>();  
 	    HashMap<String, String> formData = new HashMap<>(); 
 	    
-	    Response loginForm = Jsoup.connect(loginFormUrl).
+	    Response loginForm = Jsoup.connect(Consts.searchPage).
 	    		method(Method.GET).
-	    		userAgent(USER_AGENT).execute(); 
+	    		userAgent(Consts.userAgent).execute(); 
 	    
 	    cookies.putAll(loginForm.cookies());
 	    
 	    formData.put("login_username", Environments.username);  
 	    formData.put("login_password", Environments.password);  
 	    formData.put("two_factor", "");  
+	    formData.put("remember_me", "1");  
 
 	    Response loginResponse = 
-	    		Jsoup.connect(loginActionUrl)  
+	    		Jsoup.connect(Consts.loginPage)  
 	            .cookies(cookies)  
 	            .data(formData)  
 	            .method(Method.POST)  
-	            .userAgent(USER_AGENT)  
+	            .userAgent(Consts.userAgent)  
 	            .execute();
-	    System.out.println(loginResponse.url());
+	    if (loginResponse.cookies().containsKey(Consts.rememberMeTokenName)) {
+	    	saveCookieFile(loginResponse.cookies());
+	    }
 	    
-	    Response home = Jsoup.connect(loginFormUrl)
-		.cookies(loginResponse.cookies())
-		.userAgent(USER_AGENT)
-		.execute();
-	    
-	    System.out.println(home.url());
-	    
-	    return home.parse();
+	    return loginResponse.cookies();   
+	}
+	
+	public static void saveCookieFile(Map<String, String> cookies) {
+		try {
+	         FileOutputStream fileOut =
+	         new FileOutputStream(Consts.fileName);
+	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	         out.writeObject(cookies);
+	         out.close();
+	         fileOut.close();
+	      } catch (IOException i) {
+	         i.printStackTrace();
+	      }
+	}
+	
+	public static Map<String, String> getCookie() {
+		Map<String, String> cookies = null;
+	      try {
+	         FileInputStream fileIn = new FileInputStream(Consts.fileName);
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         cookies = (Map<String, String>) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      } catch (Exception i) {
+	         //do nothing
+	      }
+	      
+	      return cookies;
 	}
 }
