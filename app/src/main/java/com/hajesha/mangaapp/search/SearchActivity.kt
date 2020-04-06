a547cdbd69cb63dd1a19cc404d7ebbc43305906d
Hajesha <hkandasamy@ryerson.ca>
2020-04-06 03:23:29 -0400

    initialize search page add headers to the search

--- app/src/main/java/com/hajesha/mangaapp/search/SearchActivity.kt
+++ app/src/main/java/com/hajesha/mangaapp/search/SearchActivity.kt
@@ -1,6 +1,7 @@
 package com.hajesha.mangaapp.search
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.hajesha.mangaapp.R
 
@@ -8,6 +9,9 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search)
+        val layoutDemographics = findViewById(R.id.demographics) as LinearLayout
+
+
     }
 }
